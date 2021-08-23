5d8fb16dde0f89ddac46cd03f5be9bc2b8b17a4c
Max Brown <mb39@sanger.ac.uk>
2021-08-23 16:06:57 +0100

    Metadata added; tables updated

---
+++ data/apple_metadata/README.md
@@ -0,0 +1,3 @@
+# Notes
+
+- Add apple metadata here
--- src/scrape_tolqc/format_tolqc_data.R
+++ src/scrape_tolqc/format_tolqc_data.R
@@ -24,22 +24,25 @@ merged_asm <- list()
 list_index <- 1
 
 for (apple in apples) {
-    m_dom_fnames <- list.files("../../data/tolqc_data/",
+    m_dom_fnames <- list.files("../../data/tolqc_data/species_raw",
         pattern = paste(apple, "*", sep = ""),
         full.names = TRUE
     )
 
     m_dom_ls <- lapply(m_dom_fnames, fread)
 
+    # this function breaks code further down
+    # if the relative path is changed
+    # from ../../data/tolqc_data/species_raw
     names(m_dom_ls) <- gsub(
         pattern = ".txt",
         replacement = "",
-        substr(m_dom_fnames, 24, 100),
+        # 100 is some crazy upper limit...
+        substr(m_dom_fnames, 35, 100),
         fixed = TRUE
     )
 
     # all merge to gscope table
-
     gscope <- m_dom_ls[paste(apple, "_gscope_data", sep = "")][[1]]
     illumina <- m_dom_ls[paste(apple, "_ill_data", sep = "")][[1]]
     pacbio <- m_dom_ls[paste(apple, "_pbio_data", sep = "")][[1]]
@@ -54,8 +57,6 @@ for (apple in apples) {
 
     merged <- seq[gscope, on = .(source, specimen)]
 
-    merged[, apple_sp := apple]
-
     # minor cleaning
     merged <- merged[source != "-"]
 
@@ -68,31 +69,41 @@ for (apple in apples) {
 all_merged <- rbindlist(merged_list)
 all_merged_assembly <- rbindlist(merged_asm)
 
+# finally merge on apple metadata
+
+apple_metadata1 <- fread("../../data/apple_metadata/apple_key.tsv")
+
+all_merged_meta <- all_merged[apple_metadata1, on = .(specimen)]
+all_merged_assembly_meta <- all_merged_assembly[
+    apple_metadata1,
+    on = .(specimen)
+]
+
 ## WRTIE FILES
 # sequencing stats
 
 # table for stuff
-fwrite(all_merged,
-    file = "../../data/tolqc_data/all_apples_merged.tsv",
+fwrite(all_merged_meta,
+    file = "../../data/tolqc_data/tsv/all_apples_merged.tsv",
     sep = "\t"
 )
 
 # json for other stuff
 write_json(
-    x = all_merged,
-    "../../data/tolqc_data/all_apples_merged.json"
+    x = all_merged_meta,
+    "../../data/tolqc_data/json/all_apples_merged.json"
 )
 
 # assembly stats
 
 # table for stuff
-fwrite(all_merged_assembly,
-    file = "../../data/tolqc_data/all_apples_merged_asm.tsv",
+fwrite(all_merged_assembly_meta,
+    file = "../../data/tolqc_data/tsv/all_apples_merged_asm.tsv",
     sep = "\t"
 )
 
 # json for other stuff
 write_json(
-    x = all_merged_assembly,
-    "../../data/tolqc_data/all_apples_merged_asm.json"
+    x = all_merged_assembly_meta,
+    "../../data/tolqc_data/json/all_apples_merged_asm.json"
 )
